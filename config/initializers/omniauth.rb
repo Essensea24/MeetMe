b8e9e2f029d12ad684f08054406a13ad7d28a124
Cristal Chavez <cristalmchavez@yahoo.com>
2015-07-03 16:48:18 -0700

    updates users view pages

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    
-    provider :facebook, "127239617610618", "cea7ae2de6adcb47ad0fe38abc0bf5c4"
+   
+   	provider :facebook, ENV["facebook_client_id], ENV["facebook_secret"], scope: "user:email"
 
 end
