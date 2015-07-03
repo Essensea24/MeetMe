04aade35da6175cbadc1693b5fcd3de4a33c886e
Cristal Chavez <cristalmchavez@yahoo.com>
2015-07-02 17:11:33 -0700

    adds logout link

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150702215002) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "usernames", force: :cascade do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -37,6 +25,5 @@ ActiveRecord::Schema.define(version: 20150702215002) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-
 
 end
