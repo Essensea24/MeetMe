1d0945c3e1173c5bfa1ebfb441a1672525fc9e02
Cristal Chavez <cristalmchavez@yahoo.com>
2015-07-06 19:45:45 -0700

    adds devise gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.2.1'
+# ruby '2.2.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
 # Use postgresql as the database for Active Record
@@ -41,6 +41,8 @@ gem "figaro"
 
 gem "omniauth-facebook"
 gem 'omniauth-google-oauth2'
+
+gem 'devise'
 
 group :development, :test do
   gem 'rspec-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_secure_password
 	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }, :default_url =>"tobi.JPG"
     	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
